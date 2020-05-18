9e962479806e3a823c197916f5103ce3535b840e
moaz_home <moazHome@gmail.com>
2020-05-18 09:21:05 +0200

    header v-1

--- dist/file.js/app.js
+++ dist/file.js/app.js
@@ -1,4 +1,20 @@
 /*global console, alert, swal, prompt, $*/
-window.onload = function () {
-  'use strict';
-}
+$(document).ready(function() {
+  // func scroll
+  $(window).scroll(function () { 
+    // when page scrollDown 
+    if (this.scrollY > 75) {
+      $('nav').addClass('w-100 fixed-top shadow-sm');
+      $('nav').css('background', '#fff');
+      $('.nav-item:last-of-type').addClass('bg-btn')
+      $('.nav-item:last-of-type a').css('color', '#fff')
+      $('.pre-header').css('margin-top', '70px')
+    } else {
+      $('nav').removeClass('w-100 fixed-top shadow-sm');      
+      $('nav').css('background', 'transparent'); 
+      $('.nav-item:last-of-type').removeClass('bg-btn');
+      $('.nav-item:last-of-type a').css('color', '#000')
+      $('.pre-header').css('margin-top', '0px')
+    }
+  });
+})
